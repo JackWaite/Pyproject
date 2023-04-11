229b1a805e2d175c8a43d7f4dba13522a8a01097
Jack Waite <hardgimbarrforever@gmail.com>
2023-04-11 14:19:12 +0300

    Add function docstring and new function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def capital_letters(input_string):
     """String case conversion from input"""
     return input_string.upper()
+
+
+def title_letters(input_str_2):
+    """Converting the first letters in a string"""
+    return input_str_2.title()
